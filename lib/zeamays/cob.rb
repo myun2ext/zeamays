411aaadded5c0de628cc8bcb2d98e147fc02cce9
myun2ext <myun2@mbm.nifty.com>
2014-07-08 18:56:05 +0900

    Update cob.rb
    require and includes (def grow)

--- lib/zeamays/cob.rb
+++ lib/zeamays/cob.rb
@@ -1,4 +1,14 @@
+require 'zeamays/cob/gene'
+require 'zeamays/cob/base'
+require 'zeamays/cob/freezing'
+
 module Zeamays
   class Cob
+    #def grow(corns)
+    #end
+
+    include Gene
+    include Base
+    include Freezing
   end
 end
