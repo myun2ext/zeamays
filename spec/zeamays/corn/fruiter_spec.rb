0b1d36ce68444b6cbb0e890d2c9fcb5050f7f135
myun2ext <myun2@mbm.nifty.com>
2014-07-08 13:53:19 +0900

    Update fruiter_spec.rb
    test for over byte size.
    and fix use to `unpack`.

--- spec/zeamays/corn/fruiter_spec.rb
+++ spec/zeamays/corn/fruiter_spec.rb
@@ -1,8 +1,22 @@
 require 'zeamays/corn/fruiter'
+require 'zeamays/corn/fruiter/byte'
 
 describe Zeamays::Corn::Fruiter do
   context "Byte" do
-    subject { Zeamays::Corn::Fruiter::Byte.fruit_byte(30) }
-    it { should eq [30].pack("C") }
+    let(:packing) { Zeamays::Corn::Fruiter::Byte.fruit_byte(target_value) }
+
+    context "normally" do
+      let(:target_value) { 30 }
+      subject { packing.unpack("C").first }
+      it { should eq target_value }
+    end
+
+    context "over byte size" do
+      let(:target_value) { 300 }
+      subject { packing.unpack("C").first }
+      it { should eq (300 - 256) }
+    end
+
+    pending "Not byte type"
   end
 end
