58ee16bd2972d7e406361b8983643040ee6ce0e2
myun2ext <myun2@mbm.nifty.com>
2014-07-08 13:29:04 +0900

    test for Zeamays::Corn::Fruiter::Byte

--- spec/zeamays/corn/fruiter_spec.rb
+++ spec/zeamays/corn/fruiter_spec.rb
@@ -1,3 +1,8 @@
 require 'zeamays/corn/fruiter'
 
-describe Zeamays::Corn::Fruiter
+describe Zeamays::Corn::Fruiter do
+  context "Byte" do
+    subject { Zeamays::Corn::Fruiter::Byte.fruit_byte(30) }
+    it { should eq [30].pack("C") }
+  end
+end
