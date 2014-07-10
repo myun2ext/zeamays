dbf159963a9261fd2fd851109e219108d2424bf3
myun2ext <myun2@mbm.nifty.com>
2014-07-10 12:45:57 +0900

    Create string.rb

---
+++ lib/zeamays/corn/fruiter/string.rb
@@ -0,0 +1,22 @@
+module Zeamays
+  class Corn
+    class Fruiter
+      module String
+        def self.fruit_short_string(s)
+          bytesize = 2.bytesize
+          [bytesize].pack("C") + s
+        end
+
+        def self.fruit_medium_string(s)
+          bytesize = 2.bytesize
+          [bytesize].pack("n") + s
+        end
+
+        def self.fruit_large_string(s)
+          bytesize = 2.bytesize
+          [bytesize].pack("N") + s
+        end
+      end
+    end
+  end
+end
