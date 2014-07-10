5c731bab4ae77f274578279c93b6497205047f47
myun2ext <myun2@mbm.nifty.com>
2014-07-10 19:23:35 +0900

    Freezing: packaging

--- lib/zeamays/cob/freezing.rb
+++ lib/zeamays/cob/freezing.rb
@@ -1,9 +1,22 @@
 module Zeamays
   class Cob
     module Freezing
-      def freeze
+      def pack
         @rows.collect { |row|
-          row.pack(pattern)
+          row.pack(package_pattern)
+        }.join("")
+      end
+
+      private
+      def package_pattern
+        @gene.map { |type|
+          case type
+            when :i8 then 'C'
+            when :i16 then 'n'
+            when :i32 then 'N'
+            when :integer then 'N'
+            when :string then 'A'
+          end
         }.join("")
       end
     end
