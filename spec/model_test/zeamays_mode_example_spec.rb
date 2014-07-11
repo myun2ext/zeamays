b622466966ed740236663e2d725a45a96d86a7e1
myun2 <myun2@nwhite.info>
2014-07-11 17:01:18 +0900

    mock model example spec

--- spec/model_test/zeamays_mode_example_spec.rb
+++ spec/model_test/zeamays_mode_example_spec.rb
@@ -1,9 +1,22 @@
 require "zeamays"
 
 describe "Zeamays Model example" do
-  let(:mock_model) {
-    class ZeamaysMockModel < Zeamays::Cob
+  let(:example_cob_class) {
+    class ExampleCobClass < Zeamays::Cob
+      gene_sequencing :i8, :i16, :integer, :string
     end
+    ExampleCobClass
   }
-  it { mock_model }
+
+  let(:cob) { example_cob_class.new }
+  let(:record1) { [30, 2000, 500000, "test"] }
+  let(:record2) { [80, 1000, 200000, "Example!!! "] }
+  before do
+    cob.grow(record1)
+    cob.grow(record2)
+  end
+
+  let(:packaged) { cob.pack }
+  let(:unpackaged) { example_cob_class.unpack(packaged) }
+  it { unpackaged.should eq [record1, record2] }
 end
