03fe7efede58cf5f0493661b3816f8c930daad85
myun2ext <myun2@mbm.nifty.com>
2014-07-11 14:01:08 +0900

    Update defreezing.rb
    defreezing implemented

--- lib/zeamays/cob/defreezing.rb
+++ lib/zeamays/cob/defreezing.rb
@@ -1,8 +1,19 @@
 module Zeamays
   class Cob
     module Defreezing
-      def unpack(str)
-        str.unpack(package_pattern)
+      def unpack(packed_string)
+        unpacked_list = []
+
+        left_string = packed_string
+        while left_string.size != 0 do
+          unpacked = left_string.unpack(package_pattern + tail_pattern)
+          left_string = unpacked.pop
+          unpacked_list << unpacked
+        end
+      end
+
+      private def tail_pattern
+        'a*'
       end
     end
   end
