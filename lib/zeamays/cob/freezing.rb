d4c422436e52c51e09f778142b79d2d5ca1ef576
myun2 <myun2@nwhite.info>
2014-07-11 18:05:13 +0900

    package_pattern method to Class method

--- lib/zeamays/cob/freezing.rb
+++ lib/zeamays/cob/freezing.rb
@@ -3,22 +3,26 @@ module Zeamays
     module Freezing
       def pack
         @rows.collect { |row|
-          row.pack(package_pattern)
+          row.pack(self.class.package_pattern)
         }.join("")
       end
 
-      private
-      def package_pattern
-        self.class.gene_sequence.map { |type|
-          case type
-            when :i8 then 'C'
-            when :i16 then 'n'
-            when :i32 then 'N'
-            when :integer then 'N'
-            #when :string then 'A*'
-            when :string then 'Z*'
-          end
-        }.join("")
+      module ClassMethods
+        def package_pattern
+          gene_sequence.map { |type|
+            case type
+              when :i8 then 'C'
+              when :i16 then 'n'
+              when :i32 then 'N'
+              when :integer then 'N'
+              when :string then 'Z*'
+            end
+          }.join("")
+        end
+      end
+
+      def self.included(me)
+        me.extend ClassMethods
       end
     end
   end
