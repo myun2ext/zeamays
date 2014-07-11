142230555d93123e80e90bf5700bcae618d6de6f
myun2ext <myun2@mbm.nifty.com>
2014-07-11 17:12:58 +0900

    Update cob.rb
    Cob extend Defreezing

--- lib/zeamays/cob.rb
+++ lib/zeamays/cob.rb
@@ -1,11 +1,13 @@
 require 'zeamays/cob/gene'
 require 'zeamays/cob/growth'
 require 'zeamays/cob/freezing'
+require 'zeamays/cob/defreezing'
 
 module Zeamays
   class Cob
     extend Gene
     include Growth
     include Freezing
+    extend Defreezing
   end
 end
