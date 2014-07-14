d7f237e6236a44bd3138a029c5d13107707c0ce2
myun2 <myun2@nwhite.info>
2014-07-14 19:09:32 +0900

    packaging template "C"

--- lib/zeamays/corn/types/byte.rb
+++ lib/zeamays/corn/types/byte.rb
@@ -2,10 +2,15 @@ module Zeamays
   class Corn
     module Types
       class Byte
-        attr_reader :value
         @value = 0
+        attr_reader :value
+
         def initialize(value)
           @value = value
+        end
+
+        def packaging_template
+          "C"
         end
       end
     end
