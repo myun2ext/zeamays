2ee6a5c2904caa7ed286f51ee36a0c6ec7cfc7cd
myun2 <myun2@nwhite.info>
2014-07-14 14:22:50 +0900

    Fridge.take method

--- lib/zeamays/fridge.rb
+++ lib/zeamays/fridge.rb
@@ -1,9 +1,17 @@
+require 'zeamays/cob'
+
 module Zeamays
   class Fridge
     def self.stick(cob, path)
-      File.open(path, "wb") { |file|
-        f.write cob.freeze
+      File.open(path, "wb") { |f|
+        f.write(cob.freeze)
       }
+    end
+
+    def self.take(path)
+      #File.open(path, "rb") { |f|
+      #}
+      readed = File.binread(path)
     end
   end
 end
