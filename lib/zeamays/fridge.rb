cafd1be21627128ce452dc3908f34d286f8e8d41
myun2 <myun2@nwhite.info>
2014-07-15 14:24:14 +0900

    Fridge file Lock

--- lib/zeamays/fridge.rb
+++ lib/zeamays/fridge.rb
@@ -2,14 +2,16 @@ module Zeamays
   class Fridge
     def self.stick(path, freezed)
       File.open(path, "wb") { |f|
+        f.flock(File::LOCK_EX)
         f.write(freezed)
       }
     end
 
     def self.take(path)
-      #File.open(path, "rb") { |f|
-      #}
-      readed = File.binread(path)
+      File.open(path, "rb") { |f|
+        f.flock(File::LOCK_EX)
+        f.read
+      }
     end
   end
 end
